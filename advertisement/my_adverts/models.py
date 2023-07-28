ff1b729ae3448b14a16cebccbaf41ba3c2bf3652
rrrr00 <137030003+rrrr00@users.noreply.github.com>
2023-07-28 07:54:03 +0300

    Add files via upload

--- advertisement/my_adverts/models.py
+++ advertisement/my_adverts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Advertisement(models.Model):
+    title = models.CharField("Заголовок", max_length=128)
+    description = models.TextField("Описание")
+    price = models.DecimalField("Цена", max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.SmallIntegerField("Категория")
+    author = models.CharField("Автор", max_length=20)
+    location = models.CharField("Локация товара", max_length=255)
+    auction = models.BooleanField("Торг", help_text="Отметьте, если торг уместен")
+    def __str__(self): 
+        return str(self.id), self.title or '', str(self.price)
+    
+    class Meta:
+        db_table = "advertisements"
